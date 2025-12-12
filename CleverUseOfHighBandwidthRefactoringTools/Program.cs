2c6a77e0c2d5289331e31fee723895ff6ef5d168
Max Guernsey, III <max@hexsw.com>
2025-12-12 09:55:35 -0800

    .

--- CleverUseOfHighBandwidthRefactoringTools/Program.cs
+++ CleverUseOfHighBandwidthRefactoringTools/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// MIT License
+// 
+// Copyright (c) 2025-2025 Producore LLC
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using Chapter_HighBandwidthRefactoringTools;
+using Microsoft.Extensions.Configuration;
+
+var Config = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+  .AddEnvironmentVariables()
+  .Build();
+
+Console.WriteLine("""
+                  ALERT: This is not an endorsement of WeatherStack.com.
+                  
+                  I have no relationship with them whatsoever.
+                  
+                  I selected them based on the following criteria:
+                    1. Their platform works
+                    2. Their platform was free at the time I wrote "The Other Half of Coding"
+                    
+                  Use at your own risk.
+                  
+                  ---
+                  
+                  """);
+
+var Key = Config["WeatherStack:AppKey"] ?? throw new ApplicationException("Missing a key. Get one from weatherstack.com");
+var Service = new WeatherManager(Key);
+
+var MyLocation = new Site("89138");
+
+await Service.Poll(MyLocation);
---
+++ CleverUseOfHighBandwidthRefactoringTools/WeatherManager.cs
@@ -0,0 +1,127 @@
+﻿// MIT License
+// 
+// Copyright (c) 2025-2025 Hexagon Software LLC
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace Chapter_HighBandwidthRefactoringTools;
+
+public sealed class WeatherResponse
+{
+  [JsonPropertyName("current")]
+  public CurrentWeather Current { get; set; } = default!;
+}
+
+public sealed class CurrentWeather
+{
+  [JsonPropertyName("temperature")] public int Temperature { get; set; }
+}
+
+class Site(string Zip)
+{
+  public bool IsInEmergencyState { get; private set; }
+  public string Zip { get; } = Zip;
+
+  public Task ActivateEmergencyHeating()
+  {
+    Console.WriteLine("Activating emergency heating!");
+    return Task.CompletedTask;
+  }
+
+  public Task ActivateEmergencyCooling()
+  {
+    Console.WriteLine("Activating emergency cooling!");
+
+    return Task.CompletedTask;
+  }
+
+  public Task DeactivateEmergencyHeating()
+  {
+    Console.WriteLine("Deactivating emergency heating!");
+    return Task.CompletedTask;
+  }
+
+  public Task DeactivateEmergencyCooling()
+  {
+    Console.WriteLine("Deactivating emergency cooling!");
+
+    return Task.CompletedTask;
+  }
+
+  public Task AlertEmergencyConditions()
+  {
+    Console.WriteLine("ALERT: Emergency conditions!");
+    IsInEmergencyState = true;
+
+    return Task.CompletedTask;
+  }
+
+  public Task AllClear()
+  {
+    Console.WriteLine("ALERT: All clear!");
+    IsInEmergencyState = false;
+
+    return Task.CompletedTask;
+  }
+}
+
+class WeatherManager(string Key)
+{
+  public async Task Poll(Site Site)
+  {
+    var AppKey = Key;
+    using var Client = new HttpClient();
+    using var Response = await Client.GetAsync(
+      $"http://api.weatherstack.com/current" +
+      $"?access_key={AppKey}" +
+      $"&query={Site.Zip}" +
+      $"&units=f");
+
+    Response.EnsureSuccessStatusCode();
+
+    var WeatherResponse =
+      (await Response.Content.ReadFromJsonAsync<WeatherResponse>())!;
+
+    var CurrentWeather = WeatherResponse.Current;
+    var Temp = CurrentWeather.Temperature;
+
+    switch (Temp)
+    {
+      case < 55:
+        await Site.ActivateEmergencyHeating();
+        await Site.AlertEmergencyConditions();
+        break;
+      case > 85:
+        await Site.ActivateEmergencyCooling();
+        await Site.AlertEmergencyConditions();
+        break;
+      default:
+        if (Site.IsInEmergencyState)
+        {
+          await Site.DeactivateEmergencyCooling();
+          await Site.DeactivateEmergencyHeating();
+        }
+        await Site.AllClear();
+        break;
+    }
+  }
+}
